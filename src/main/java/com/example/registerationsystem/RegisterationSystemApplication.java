22452601a02b97d662e3b5b66ddb8d71239ccf60
Safwatzzz <42112466+Ahmedsafwat101@users.noreply.github.com>
2022-02-26 18:30:44 +0200

    Initial commit

---
+++ src/main/java/com/example/registerationsystem/RegisterationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.registerationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/registerationsystem/RegisterationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registerationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
